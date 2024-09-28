26e0f4a8781ae4bafb8b0f4cdb43d9f1bb5f1fa7
Ryan Atkinson <mali[D[Cmail@ryanatkn.com>
2024-09-28 15:25:46 -0600

    include dist

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare const works_when_same_name_exported: "works_when_same_name";
+export declare const broken_when_different_name_exported: any;
+export declare const broken_when_different_path_exported: any;
